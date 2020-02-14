1555a433c5c6555a7b0c58b8ed03b1738dd81622
Mathia Haure-Touze <mathia.haure-touzet.ext@francetv.fr>
2020-02-14 14:03:47 +0100

    add py_amqp_worker requirements

--- worker.py
+++ worker.py
@@ -1,19 +1,18 @@
 import ftv_facerec
+import logging
 
+logging.basicConfig(level=logging.ERROR)
 
 def get_name():
-    return "Face recognition worker"
+    return ftv_facerec.name
 
 
 def get_short_description():
-    return "Process face recognition on a video"
+    return ftv_facerec.short_description
 
 
 def get_description():
-    return """Retrieve a json file from an video.
-        The input requires:
-        - a video
-        """
+    return ftv_facerec.description
 
 
 def get_version():
@@ -28,10 +27,26 @@ def get_parameters():
             "kind": ["string"]
         },
         {
+            "identifier": "persons_path",
+            "label": "Json file representing person to detect",
+            "kind": ["object"]
+        },
+        {
             "identifier": "destination_path",
             "label": "Destination Path",
             "kind": ["string"]
-        }
+        },
+        {
+            "identifier": "provider",
+            "label": "Provider name",
+            "kind": ["string"]
+        },
+        {
+            "identifier": "sample_rate",
+            "label": "Specify the rate of analysed frame",
+            "kind": ["integer"]
+        },
+
     ]
 
 
